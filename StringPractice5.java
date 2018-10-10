a6915fb60f66f38b95392be45dbbc94ea9c7ceb5
Jay sanjay Fursule <jayesh.f1@gmail.com>
2018-10-10 21:47:31 +0530

    Add files via upload

---
+++ StringPractice5.java
@@ -0,0 +1,15 @@
+
+
+
+class StringPractice5
+{
+	public static void main(String args[])
+	{
+		String str = "JAVA";
+		String str1 = new String ("JAVA");
+		
+		System.out.println(str.equals(str1));
+		System.out.println(str == str1);
+		System.out.println(str.compareTo(str1));
+	}
+}
